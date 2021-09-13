85391a7f8c4a90b0a05c006c68f043d3d4289e9e
nhamilton1 <nickh3592@gmail.com>
2021-09-13 17:30:39 -0400

    completed question 5

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -22,6 +22,16 @@ const testShow = {
             name: 'season 1',
             episodes: [],
         },
+        {
+            id: 2,
+            name: 'season 2',
+            episodes: [],
+        },
+        {
+            id: 3,
+            name: 'season 3',
+            episodes: [],
+        },
 
     ],
 }
@@ -48,6 +58,22 @@ test('when the fetch button is pressed, the show component will display', async 
 
 })
 
+test('when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in data', async () => {
+    fetchShow.mockResolvedValueOnce(testShow)
+
+    //1. ARRANGE
+    render(<Display/>)
+
+    //2. ACT
+    const button = screen.getByRole('button')
+    userEvent.click(button)
+
+    //3. ASSERT
+    const seasonOptions = await screen.findAllByTestId('season-option')
+    expect(seasonOptions).toHaveLength(4)
+
+})
+
 
 
 
@@ -55,6 +81,6 @@ test('when the fetch button is pressed, the show component will display', async 
 //1. Add in nessisary imports and values to establish the testing suite. x
 //2. Test that the Display component renders without any passed in props. x
 //3. Rebuild or copy a show test data element as used in the previous set of tests. x
-//4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
+//4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.x
 //5. Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.
 //6. Notice the optional functional prop passed in to the Display component client code. Test that when the fetch button is pressed, this function is called.
