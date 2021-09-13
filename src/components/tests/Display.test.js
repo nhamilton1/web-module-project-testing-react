7b052694da5f4cc3d57c2743330dcc519a931330
nhamilton1 <nickh3592@gmail.com>
2021-09-13 18:07:40 -0400

    finished question 6 during office hours

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Display from '../Display';
 
@@ -74,6 +74,23 @@ test('when the fetch button is pressed, the amount of select options rendered is
 
 })
 
+test('Test that when the fetch button is pressed, the displayFunc function is called', async () => {
+    fetchShow.mockResolvedValueOnce(testShow)
+    const displayFunc = jest.fn()
+
+    //1. ARRANGE
+    render(<Display displayFunc={displayFunc}/>)
+
+    //2. ACT
+    const button = screen.getByRole('button')
+    userEvent.click(button)
+
+    //3. ASSERT
+    await waitFor(() => {
+        expect(displayFunc).toHaveBeenCalled()
+    })
+
+})
 
 
 
@@ -82,5 +99,5 @@ test('when the fetch button is pressed, the amount of select options rendered is
 //2. Test that the Display component renders without any passed in props. x
 //3. Rebuild or copy a show test data element as used in the previous set of tests. x
 //4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.x
-//5. Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.
+//5. Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.x
 //6. Notice the optional functional prop passed in to the Display component client code. Test that when the fetch button is pressed, this function is called.
